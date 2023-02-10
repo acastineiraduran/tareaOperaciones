bc5fde90c1de748671d06e5efbaf4d9fe026d160
acastineiraduran <acastineiraduran@danielcastelao.org>
2023-02-11 00:17:21 +0100

    todo bien, solo falta modificar NaN e Infinity

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        Metodos obx = new Metodos();
 
+
+        Float num1 = EntradaSalida.entradaFloat(Metodos.COMENTARIO);
+        Float num2 = EntradaSalida.entradaFloat(Metodos.COMENTARIO);
+        if (num1 == null || num2 ==null){
+            System.out.println("ERROR: NUMEROS INCORRECTOS");
+        }else {
+
+            Float resultado = obx.operacion(num1,
+                    num2,
+                    4);
+
+            if (resultado != null) {
+                System.out.println(resultado);
+            } else {
+                System.out.println("***ERROR EN CONSOLA***");
+            }
+        }
     }
 }
--- src/Metodos.java
+++ src/Metodos.java
@@ -1,3 +1,43 @@
 public class Metodos {
-
+    final static int SUMA = 1;
+    final static int RESTA = 2;
+    final static int MULTI = 3;
+    final static int DIV = 4;
+    final static String COMENTARIO = "Introduce un numero";
+    public static Float operacion (float num1, float num2, int opcion) {
+        Float resultado = null;
+            switch (opcion) {
+                case SUMA:
+                    try {
+                        resultado = num1 + num2;
+                    }catch (Exception e){
+                        return null;
+                    }
+                    break;
+                case RESTA:
+                    try {
+                        resultado = num1 - num2;
+                    }catch (Exception e){
+                        return null;
+                    }
+                    break;
+                case MULTI:
+                    try {
+                        resultado = num1 * num2;
+                    }catch (Exception e){
+                        return null;
+                    }
+                    break;
+                case DIV:
+                    try {
+                        resultado = num1 / num2;
+                    }catch (Exception e){
+                        return null;
+                    }
+                    break;
+                default:
+                    return null;
+            }
+        return resultado;
+    }
 }
